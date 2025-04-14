a830df21ea4d4dba2d05fb60cbdd184d48223a83
kalkidan-teklay <kalkidan.teklehaimanot@aait.edu.et>
2025-04-14 11:36:19 +0300

    Time: 2787 ms (9.16%), Space: 17.8 MB (63.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,17 +1,15 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        length = len(s1)
         freq = defaultdict(int)
         freq2 = defaultdict(int)
-        check = False
         for ch in s1:
             freq[ch] +=1
-        for i in range(len(s2)- length +1):
-            window = s2[i: length+i]
+        for i in range(len(s2)- len(s1) +1):
+            window = s2[i: len(s1)+i]
             for j in window:
                 freq2[j] += 1
                 if freq == freq2:
-                    check = True
+                    return True
             freq2.clear()
-        return check
+        return False
         
